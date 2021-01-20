ed6902ac29ede9745ab7b05b5ac46797f49699a1
Ricardo Lopez <a01066515@itesm.mx>
2021-01-20 11:50:14 -0600

    assets

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ external_stylesheets = [
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css'
 ]
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets= external_stylesheets)
+app.title = 'Monitoreo COVID-19'
 server = app.server
